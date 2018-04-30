b9944f29a623aad5470a24d2f4c6b84440e5bdef
Adnan Sawas <adnsawas@gmail.com>
2018-04-30 21:19:09 +0300

    Added collision, palayer control and winning and losing and score

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(index) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.speedX = Math.floor(Math.random() * 4) + 1;
+    this.x = -100;
+    this.y = enemiesStartYpositions[Math.floor(Math.random() * 3)];
+    this.index = index;
+
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +19,22 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += dt * this.speedX * 100;
+    if (this.x > 550) {
+        delete this;
+    }
+
+    // Losing Condition
+    // Detecting a collision between this enemy and the player
+    if (Math.abs(player.x - this.x) < 75 && Math.abs(player.y - this.y) < 80 ) {
+        // Reset player position
+        player.x = 200;
+        player.y = 380;
+        scoreLabel.innerText = --score;
+        console.log("You hit an enemy");
+
+    }
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +45,77 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 380;
+}
+
+Player.prototype.update = function(dt) {
+    // Winning Condition
+    if (player.y <= 35)
+    {
+        player.x = 200;
+        player.y = 380;
+        scoreLabel.innerText = ++score;
+    }
+
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(key) {
+    const xStep = 101;
+    const yStep = 83;
+    if (key == 'up') {
+        if (player.y > -35) {
+            player.y -= yStep;
+        }
+    }
+    else if (key == 'down') {
+        if (player.y < 380) {
+            player.y += yStep;
+        }
+    }
+    else if (key == 'right') {
+        if (player.x < 402) {
+            player.x += xStep;
+        }
+    }
+    else if (key == 'left') {
+        if (player.x > -2) {
+            player.x -= xStep;
+        }
+    }
+
+    console.log(player.x, player.y);
+}
+
+
+// Game Variables
+const enemiesStartYpositions = [48, 131, 214];
+let score = 0;
+let scoreLabel;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    scoreLabel = document.getElementById("score");
+  });
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+let allEnemies = [];
+
+let interval = setInterval(generateEnemy, 1100);
 // Place the player object in a variable called player
+let player = new Player();
 
-
+function generateEnemy() {
+    if (allEnemies.length == 20) {allEnemies.splice(0, 10);}
+    allEnemies.push(new Enemy(allEnemies.length));
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
